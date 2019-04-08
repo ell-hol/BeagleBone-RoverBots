0625e869d1898b09f4df1aee0fb1ace0e194fd93
Yacine Zahidi <yacine.zahidi@insa-lyon.fr>
2019-04-08 11:07:34 +0200

    Update lineFollower_NoMovement.py

--- lineFollower_NoMovement.py
+++ lineFollower_NoMovement.py
@@ -11,6 +11,9 @@ while(True):
     # Capture the frames
     ret, frame = video_capture.read()
 
+    #image flip
+    frame = cv2.flip(frame, 1)
+
     # Crop the image
     crop_img = frame[60:120, 0:160]
 
@@ -24,15 +27,18 @@ while(True):
     ret,thresh = cv2.threshold(blur,60,255,cv2.THRESH_BINARY_INV)
 
     # Find the contours of the frame
-    contours,hierarchy = cv2.findContours(thresh.copy(), 1, cv2.CHAIN_APPROX_NONE)
+    _, contours,hierarchy = cv2.findContours(thresh.copy(), 1, cv2.CHAIN_APPROX_NONE)
 
     # Find the biggest contour (if detected)
     if len(contours) > 0:
         c = max(contours, key=cv2.contourArea)
         M = cv2.moments(c)
 
-        cx = int(M['m10']/M['m00'])
-        cy = int(M['m01']/M['m00'])
+        try:
+            cx = int(M['m10']/M['m00'])
+            cy = int(M['m01']/M['m00'])
+        except ZeroDivisionError as e:
+            print(e)
 
         cv2.line(crop_img,(cx,0),(cx,720),(255,0,0),1)
         cv2.line(crop_img,(0,cy),(1280,cy),(255,0,0),1)
@@ -55,3 +61,5 @@ while(True):
     cv2.imshow('frame',crop_img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+video_capture.release()
